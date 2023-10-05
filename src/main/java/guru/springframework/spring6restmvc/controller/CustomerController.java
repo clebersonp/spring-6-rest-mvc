63df0abe1814cb2db0fcb2dd53e91584d9266d75
Cleberson <pauluci.cleberson@gmail.com>
2023-10-04 22:27:14 -0300

    Assignment done. Post handler for customer

--- src/main/java/guru/springframework/spring6restmvc/controller/CustomerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/CustomerController.java
@@ -2,14 +2,18 @@ package guru.springframework.spring6restmvc.controller;
 
 import guru.springframework.spring6restmvc.model.Customer;
 import guru.springframework.spring6restmvc.services.CustomerService;
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.UUID;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -19,16 +23,24 @@ import java.util.UUID;
 @RestController
 public class CustomerController {
 
-    private final CustomerService customerService;
+  private final CustomerService customerService;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Customer> listAllCustomers(){
-        return customerService.getAllCustomers();
-    }
+  @RequestMapping(method = RequestMethod.GET)
+  public List<Customer> listAllCustomers() {
+    return customerService.getAllCustomers();
+  }
 
-    @RequestMapping(value = "{customerId}", method = RequestMethod.GET)
-    public Customer getCustomerById(@PathVariable("customerId") UUID id){
-        return customerService.getCustomerById(id);
-    }
+  @RequestMapping(value = "{customerId}", method = RequestMethod.GET)
+  public Customer getCustomerById(@PathVariable("customerId") UUID id) {
+    return customerService.getCustomerById(id);
+  }
+
+  @PostMapping
+  public ResponseEntity<?> handlePost(@RequestBody Customer customer) {
+    Customer savedCustomer = this.customerService.saveNewCustomer(customer);
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest().pathSegment("{customerId}")
+        .build(savedCustomer.getId());
+    return ResponseEntity.created(location).build();
+  }
 
 }
--- src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceImpl.java
@@ -1,10 +1,13 @@
 package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Customer;
-import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -12,48 +15,41 @@ import java.util.*;
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
-    private Map<UUID, Customer> customerMap;
+  private Map<UUID, Customer> customerMap;
 
-    public CustomerServiceImpl() {
-        Customer customer1 = Customer.builder()
-                .id(UUID.randomUUID())
-                .name("Customer 1")
-                .version(1)
-                .createdDate(LocalDateTime.now())
-                .updateDate(LocalDateTime.now())
-                .build();
+  public CustomerServiceImpl() {
+    Customer customer1 = Customer.builder().id(UUID.randomUUID()).name("Customer 1").version(1)
+        .createdDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build();
 
-        Customer customer2 = Customer.builder()
-                .id(UUID.randomUUID())
-                .name("Customer 2")
-                .version(1)
-                .createdDate(LocalDateTime.now())
-                .updateDate(LocalDateTime.now())
-                .build();
+    Customer customer2 = Customer.builder().id(UUID.randomUUID()).name("Customer 2").version(1)
+        .createdDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build();
 
-        Customer customer3 = Customer.builder()
-                .id(UUID.randomUUID())
-                .name("Customer 3")
-                .version(1)
-                .createdDate(LocalDateTime.now())
-                .updateDate(LocalDateTime.now())
-                .build();
+    Customer customer3 = Customer.builder().id(UUID.randomUUID()).name("Customer 3").version(1)
+        .createdDate(LocalDateTime.now()).updateDate(LocalDateTime.now()).build();
 
-        customerMap = new HashMap<>();
-        customerMap.put(customer1.getId(), customer1);
-        customerMap.put(customer2.getId(), customer2);
-        customerMap.put(customer3.getId(), customer3);
-    }
+    customerMap = new HashMap<>();
+    customerMap.put(customer1.getId(), customer1);
+    customerMap.put(customer2.getId(), customer2);
+    customerMap.put(customer3.getId(), customer3);
+  }
 
-    @Override
-    public Customer getCustomerById(UUID uuid) {
-        return customerMap.get(uuid);
-    }
+  @Override
+  public Customer getCustomerById(UUID uuid) {
+    return customerMap.get(uuid);
+  }
 
-    @Override
-    public List<Customer> getAllCustomers() {
-        return new ArrayList<>(customerMap.values());
-    }
+  @Override
+  public List<Customer> getAllCustomers() {
+    return new ArrayList<>(customerMap.values());
+  }
+
+  @Override
+  public Customer saveNewCustomer(Customer customer) {
+    Customer savedCustomer = Customer.builder().id(UUID.randomUUID()).createdDate(LocalDateTime.now())
+        .updateDate(LocalDateTime.now()).version(1).name(customer.getName()).build();
+    this.customerMap.put(savedCustomer.getId(), savedCustomer);
+    return savedCustomer;
+  }
 }
 
 
