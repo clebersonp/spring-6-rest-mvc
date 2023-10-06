c47fb43e2f2c25342383e9a6fe26a1d261639e45
Cleberson <pauluci.cleberson@gmail.com>
2023-10-06 00:47:32 -0300

    Created customer controller method test for update the customer

--- src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
@@ -1,5 +1,18 @@
 package guru.springframework.spring6restmvc.controller;
 
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.spring6restmvc.model.Customer;
 import guru.springframework.spring6restmvc.services.CustomerService;
@@ -12,72 +25,81 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.core.Is.is;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 @WebMvcTest(CustomerController.class)
 class CustomerControllerTest {
 
-    @MockBean
-    CustomerService customerService;
+  @MockBean
+  CustomerService customerService;
 
-    @Autowired
-    MockMvc mockMvc;
+  @Autowired
+  MockMvc mockMvc;
 
-    @Autowired
-    ObjectMapper objectMapper;
+  @Autowired
+  ObjectMapper objectMapper;
 
-    CustomerServiceImpl customerServiceImpl;
+  CustomerServiceImpl customerServiceImpl;
 
-    @BeforeEach
-    void setUp() {
-        customerServiceImpl = new CustomerServiceImpl();
-    }
+  @BeforeEach
+  void setUp() {
+    customerServiceImpl = new CustomerServiceImpl();
+  }
 
-    @Test
-    void testCreatCustomer() throws Exception {
-        Customer customer = customerServiceImpl.getAllCustomers().get(0);
-        customer.setId(null);
-        customer.setVersion(null);
+  @Test
+  void testCreatCustomer() throws Exception {
+    Customer customer = customerServiceImpl.getAllCustomers().get(0);
+    customer.setId(null);
+    customer.setVersion(null);
 
-        given(customerService.saveNewCustomer(any(Customer.class)))
-                .willReturn(customerServiceImpl.getAllCustomers().get(1));
+    given(customerService.saveNewCustomer(any(Customer.class)))
+        .willReturn(customerServiceImpl.getAllCustomers().get(1));
 
-        mockMvc.perform(post("/api/v1/customer").contentType(MediaType.APPLICATION_JSON)
+    mockMvc.perform(post("/api/v1/customer").contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(customer)))
+        .andExpect(status().isCreated())
+        .andExpect(header().exists("Location"));
+  }
+
+  @Test
+  void listAllCustomers() throws Exception {
+    given(customerService.getAllCustomers()).willReturn(customerServiceImpl.getAllCustomers());
+
+    mockMvc.perform(get("/api/v1/customer")
+            .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.length()", is(3)));
+  }
+
+  @Test
+  void getCustomerById() throws Exception {
+    Customer customer = customerServiceImpl.getAllCustomers().get(0);
+
+    given(customerService.getCustomerById(customer.getId())).willReturn(customer);
+
+    mockMvc.perform(get("/api/v1/customer/" + customer.getId())
+            .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.name", is(customer.getName())));
+
+  }
+
+  @Test
+  void updateCustomerById() throws Exception {
+    Customer customer = this.customerServiceImpl.getAllCustomers().get(0);
+
+    willDoNothing().given(this.customerService).updateCustomerById(customer.getId(), customer);
+
+    this.mockMvc.perform(
+            put("/api/v1/customer/%s".formatted(customer.getId().toString()))
                 .accept(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(customer)))
-                .andExpect(status().isCreated())
-                .andExpect(header().exists("Location"));
-    }
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(customer)))
+        .andExpect(status().isNoContent());
 
-    @Test
-    void listAllCustomers() throws Exception {
-        given(customerService.getAllCustomers()).willReturn(customerServiceImpl.getAllCustomers());
-
-        mockMvc.perform(get("/api/v1/customer")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.length()", is(3)));
-    }
-
-    @Test
-    void getCustomerById() throws Exception {
-        Customer customer = customerServiceImpl.getAllCustomers().get(0);
-
-        given(customerService.getCustomerById(customer.getId())).willReturn(customer);
-
-        mockMvc.perform(get("/api/v1/customer/" + customer.getId())
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.name", is(customer.getName())));
-
-    }
+    verify(this.customerService).updateCustomerById(customer.getId(), customer);
+  }
 }
 
 
